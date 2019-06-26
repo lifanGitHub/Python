71efe3f759540519d3ea77a9f888e7644b8b6abd
LiFan <494360059@qq.com>
2019-06-26 11:26:57 +0800

    优化代码

--- web/CSDNTest.py
+++ web/CSDNTest.py
@@ -1,30 +1,35 @@
 from util import NetUtil
 from util import ViewUtil
 
-sumRead = 0
-titleList = []
-numList = []
+
+def main():
+    sumRead = 0
+    titleList = []
+    numList = []
+    for indexHtml in range(1, 2):
+        soup = NetUtil.getBeautifulSoup(
+            "https://blog.csdn.net/coder_pig/article/list/%d" % indexHtml)
+        # if soup is None:
+        #     sys.exit()
+
+        items = soup.find_all(name="div",
+                              attrs={"class": "article-item-box csdn-tracking-statistics"})
+        for index, item in enumerate(items):
+            if index == 0:
+                continue
+            titleList.append(item.a.contents[2].strip())
+            print(item.a.contents[2].strip())
+
+            readNum = item.find_all(name="span", attrs={"class": "num"})[0].string
+            numList.append(int(readNum))
+            sumRead += int(readNum)
+            print("阅读数= " + readNum)
+
+    ViewUtil.showChart(titleList, numList)
 
 
-for indexHtml in range(1, 17):
-    soup = NetUtil.getBeautifulSoup("https://blog.csdn.net/coder_pig/article/list/%d" % indexHtml)
-    # if soup is None:
-    #     sys.exit()
-
-    items = soup.find_all(name="div", attrs={"class": "article-item-box csdn-tracking-statistics"})
-    for index, item in enumerate(items):
-        if index == 0:
-            continue
-        titleList.append(item.a.contents[2].strip())
-        print(item.a.contents[2].strip())
-
-        readNum = item.find_all(name="span", attrs={"class": "num"})[0].string
-        numList.append(int(readNum))
-        sumRead += int(readNum)
-        print("阅读数= " + readNum)
-
-ViewUtil.showChart(titleList,numList)
-
+if __name__ == '__main__':
+    main()
 # print(all_href[1])
 # print(all_href[1].a)
 # print(all_href[1].a.contents)
