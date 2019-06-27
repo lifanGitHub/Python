2116fcea9b6edf84e08bc804128e170ebcea5f60
LiFan <494360059@qq.com>
2019-06-27 11:01:47 +0800

    下载豆瓣图片

--- web/DouBan.py
+++ web/DouBan.py
@@ -1,21 +1,24 @@
-import os
 import urllib.request
 
-from util import NetUtil
+from bs4 import Tag
+
+from util import NetUtil, IOUtil
 
 
 def main():
     soup = NetUtil.getBeautifulSoup('https://movie.douban.com/top250?filter=')
     items = soup.find(name='ol', attrs={'class': 'grid_view'})
     for index, item in enumerate(items):
-        if index == 1:
-            imageItem = item.find(attrs={'class': 'pic'}).a.img
-            name = imageItem['alt']
-            src = imageItem['src']
-            print(name + src)
-            urllib.request.urlretrieve(src, os.path.abspath('.')+'/1.png')
+        print(type(item))
+        if not isinstance(item, Tag):
+            continue
+        imageItem = item.find(attrs={'class': 'pic'}).a.img
+        name = imageItem['alt']
+        src = imageItem['src']
+        print(name + src)
+        path = IOUtil.mk_dir('img')
+        urllib.request.urlretrieve(src, path + '%s.png' % name)
 
 
 if __name__ == '__main__':
     main()
-
